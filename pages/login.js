f24048f23a4045e73f1ce3db19c83356588c439d
iamsahebgiri <iamsahebgiri@gmail.com>
2021-01-31 17:10:36 +0530

    facebook and google oauth added

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ import {
   Text
 } from '@chakra-ui/react';
 import { FcGoogle } from 'react-icons/fc';
-import { SiGithub } from 'react-icons/si';
+import { SiGithub, SiFacebook } from 'react-icons/si';
 
 import { useAuth } from '@/lib/auth';
 
@@ -20,35 +20,19 @@ const Login = () => {
     <Container>
       <Flex direction="column" alignItems="center">
         <LincTreeIcon h={50} my={6} />
-        <Heading as="h4" size="md" my={3} textAlign="center">
-          Sign in to linctree
-        </Heading>
         <Box bg="white" width={['100%', 'sm']} shadow="sm" rounded="md" p={6}>
-          <Stack spacing={3}>
+          <Heading as="h4" size="md" my={6} mb={8} textAlign="center">
+            Sign in to Linctree
+          </Heading>
+          <Stack spacing={4}>
             <Button
-              onClick={() => auth.signinWithGitHub('/dashboard')}
-              backgroundColor="gray.900"
-              color="white"
-              fontWeight="medium"
-              leftIcon={<Icon as={SiGithub} />}
-              mt={4}
-              mr={2}
-              _hover={{ bg: 'gray.700' }}
-              _active={{
-                bg: 'gray.800',
-                transform: 'scale(0.95)'
-              }}
-            >
-              Continue with GitHub
-            </Button>
-            <Button
-              // onClick={() => auth.signinWithGoogle()}
+              isLoading={auth.loading.google}
+              onClick={() => auth.signinWithGoogle('/dashboard')}
               backgroundColor="white"
               color="gray.900"
               variant="outline"
               fontWeight="medium"
               leftIcon={<Icon as={FcGoogle} />}
-              mt={4}
               _hover={{ bg: 'gray.100' }}
               _active={{
                 bg: 'gray.100',
@@ -56,6 +40,29 @@ const Login = () => {
               }}
             >
               Continue with Google
+            </Button>
+            <Button
+              isLoading={auth.loading.facebook}
+              onClick={() => auth.signinWithFacebook('/dashboard')}
+              colorScheme="messenger"
+              leftIcon={<Icon as={SiFacebook} />}
+            >
+              Continue with Facebook
+            </Button>
+            <Button
+              isLoading={auth.loading.github}
+              onClick={() => auth.signinWithGitHub('/dashboard')}
+              backgroundColor="gray.900"
+              color="white"
+              fontWeight="medium"
+              leftIcon={<Icon as={SiGithub} />}
+              _hover={{ bg: 'gray.700' }}
+              _active={{
+                bg: 'gray.800',
+                transform: 'scale(0.95)'
+              }}
+            >
+              Continue with GitHub
             </Button>
           </Stack>
           <Text
